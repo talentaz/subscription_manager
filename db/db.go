94e69792a2c00f900a9d8aca364034452121acba
talentaz <daisadadaser@gmail.com>
2022-06-25 11:53:34 +0900

    create new branch

--- db/db.go
+++ db/db.go
@@ -8,8 +8,8 @@ import (
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
-        "gorm.io/gorm/schema"
-        "gorm.io/gorm/logger"
+	"gorm.io/gorm/logger"
+	"gorm.io/gorm/schema"
 )
 
 var DB *gorm.DB
@@ -24,18 +24,18 @@ func Init() {
 	host := config.Host
 	port := config.Port
 	dbname := config.DBName
-        dbSchema := config.DBSchema
+	dbSchema := config.DBSchema
 
 	dbURL := fmt.Sprintf("host=%s port=%d user=%s "+"password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
 
 	//db, err := gorm.Open(postgres.Open(dbURL), &gorm.Config{})
-        db, err := gorm.Open(postgres.Open(dbURL), &gorm.Config{
-                     NamingStrategy: schema.NamingStrategy{
-                       TablePrefix: dbSchema + ".",
-                       NoLowerCase: false,
-                     },
-                     Logger: logger.Default.LogMode(logger.Info),
-                   })
+	db, err := gorm.Open(postgres.Open(dbURL), &gorm.Config{
+		NamingStrategy: schema.NamingStrategy{
+			TablePrefix: dbSchema + ".",
+			NoLowerCase: false,
+		},
+		Logger: logger.Default.LogMode(logger.Info),
+	})
 
 	if err != nil {
 		panic("failed to connect database")
